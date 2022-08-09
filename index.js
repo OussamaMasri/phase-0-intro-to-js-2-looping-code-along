023cda967881fcb380c5aea763acc1946490e781
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-09 21:58:39 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names,event) {
+  let message = []
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return message;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
+
+function countDown(){
+for (let countdown = 0; countdown < 11; countdown++) {
+  console.log(countdown);
+}
+}
